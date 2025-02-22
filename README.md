d11ede2059068e197bc06930abcaec1bbd5cea0f
Salar Rahmanian <code@softinio.com>
2025-02-21 23:46:57 -0800

    cleanup and corrections to make overlays usable

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # nix-overlays
-My Nix Overlays
+
+Collection of Nix overlays and package definitions.
+
+## Available Overlays
+
+- **uv**: Python package manager and installer (version 0.6.0)
+
+## Usage
+
+You can use these overlays in your flake-based projects in several ways:
+
+### Using specific overlays
+
+```nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-overlays.url = "github:softinio/nix-overlays";
+  };
+
+  outputs = { self, nixpkgs, nix-overlays }: {
+    # Example for NixOS configuration
+    nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [
+            nix-overlays.overlays.uv
+          ];
+        })
+      ];
+    };
+  };
+}
+```
+
+### Using all overlays together
+
+```nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-overlays.url = "github:softinio/nix-overlays";
+  };
+
+  outputs = { self, nixpkgs, nix-overlays }: {
+    # Example for NixOS configuration
+    nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [
+            nix-overlays.overlays.default
+          ];
+        })
+      ];
+    };
+  };
+}
+```
+
+### Directly using packages
+
+```nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-overlays.url = "github:softinio/nix-overlays";
+  };
+
+  outputs = { self, nixpkgs, nix-overlays }:
+  let
+    system = "x86_64-linux"; # or your preferred system
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    # Example for accessing a package directly
+    packages.${system}.default = nix-overlays.packages.${system}.uv;
+  };
+}
+```
--- flake.nix
+++ flake.nix
@@ -3,50 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      allSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forAllSystems = nixpkgs.lib.genAttrs allSystems;
-
-      overlays = [ (import ./uv) ];
-      nixpkgsFor = forAllSystems (
-        system:
-        import nixpkgs {
-          inherit system;
-          overlays = overlays;
-        }
-      );
-
-    in
+  outputs = { self, nixpkgs, flake-utils }:
     {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-          python = pkgs.python313;
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              # Make sure we get the overlaid version directly
-              python
-              python.pkgs.uv # This will use the overlaid version
-              pkgs.git
-            ];
-            shellHook = ''
-              export PATH="${python}/bin:${python.pkgs.uv}/bin:$PATH"
-            '';
+      # Expose each overlay individually
+      overlays = {
+        default = nixpkgs.lib.composeManyExtensions [ self.overlays.uv ];
+        uv = final: prev: {
+          python313 = prev.python313.override {
+            packageOverrides = pyFinal: pyPrev: {
+              uv = pyFinal.callPackage ./pkgs/uv/default.nix { };
+            };
           };
-        }
-      );
-    };
+        };
+      };
+    } // 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+        python = pkgs.python313;
+      in
+      {
+        # Expose packages
+        packages = {
+          default = python.pkgs.uv;
+          uv = python.pkgs.uv;
+        };
+        
+        # Development shell
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # Make sure we get the overlaid version directly
+            python
+            python.pkgs.uv # This will use the overlaid version
+            pkgs.git
+          ];
+          shellHook = ''
+            export PATH="${python}/bin:${python.pkgs.uv}/bin:$PATH"
+          '';
+        };
+      }
+    );
 }
